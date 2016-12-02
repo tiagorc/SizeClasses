dac6e83ed48eff2cd090e0dc7c9804736ac61d9c
tiagorc <tiagobrasil2005@yahoo.com.br>
2016-12-02 16:35:09 -0200

    Initial Commit

---
+++ SizeClassesXcode8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SizeClassesXcode8
+//
+//  Created by Euler Tiago Rodrigues de Carvalho on 02/12/16.
+//  Copyright © 2016 Euler Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
